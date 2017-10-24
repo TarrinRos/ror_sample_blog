aad899604f106e5e37d44d77ce1c11422aa7ddea
TarrinRos <tarindis@gmail.com>
2017-10-24 19:35:56 +0300

    Add articles create view and articles validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
   def new
   end
+  # for POST method
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  # Premit for marams
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get '/' => 'home#index'
   resource :contacts, only: [:new, :create], path_names: { new: '' }
-  resources :articles
+  resource :articles, only: [:new, :create], path_names: { new: '' }
   resource :terms, only: [:show]
+  resource :about, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
