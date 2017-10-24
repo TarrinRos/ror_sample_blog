ce6f02850cfbfd2838c2a27910946e5783d3990b
TarrinRos <tarindis@gmail.com>
2017-10-24 13:36:45 +0300

    Saving contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,15 @@
 class ContactsController < ApplicationController
   # for GET method
-  def new
-  end
+  def new; end
+
   # for POST method
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 end
