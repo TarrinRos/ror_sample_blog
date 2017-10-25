8587ff0ea319a3d22bfcf70dc74ba469c1ad8232
TarrinRos <tarindis@gmail.com>
2017-10-25 17:16:17 +0300

    implement articles#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get '/' => 'home#index'
   resource :contacts, only: %i[new create], path_names: { new: '' }
-  resource :articles, only: %i[new create], path_names: { new: '' }
+  resources :articles
   resource :terms, only: [:show]
   resource :about, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
