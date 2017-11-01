edfb06a4c14c2b47659a6a5b7b5452d1b640cc30
TarrinRos <tarindis@gmail.com>
2017-11-01 17:23:19 +0200

    Displaying users email in the header

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_filter :authenticate_user!, except => [:show, :index]
+  before_action :authenticate_user!
   def index
     @articles = Article.all
   end
