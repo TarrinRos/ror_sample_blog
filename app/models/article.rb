584aaf4a16c1d3324bf027fc3121a541a2d9bffa
TarrinRos <tarindis@gmail.com>
2017-11-06 16:22:27 +0200

    Add Articles title length validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 140 }, allow_blank: true
   validates :text, presence: true
   has_many :comments
 
