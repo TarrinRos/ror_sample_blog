2c442d82da7f05107c80e4fb42f1ce6bd41713cb
TarrinRos <tarindis@gmail.com>
2017-11-07 15:59:17 +0200

    Scetario allows access to contacts page

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,8 +1,9 @@
-require 'spec_helper'
+require "rails_helper"
 
-feature 'Contact Creation' do
-  scenario 'allows access to contact page' do
+feature "Contact Creation" do
+  scenario "allows access to contacts page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact Us'
+
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
